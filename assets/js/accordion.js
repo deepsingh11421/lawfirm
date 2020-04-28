f05076f4a7e8d253ddb6f7475c3ad7a31382d4f7
ajachintya <40269281+ajachintya@users.noreply.github.com>
2020-04-28 20:51:12 +0530

    accordion color

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -4,6 +4,21 @@ var acc = document.getElementsByClassName("accordion");
         var i;
         for (i = 0; i < acc.length; i++) {
             acc[i].addEventListener("click", function () {
+                for(j=0;j<acc.length;j++){
+                    if(acc[j] != this){
+                        if(acc[j].classList.contains('active')){
+                            acc[j].classList.remove('active');
+                            var pan = acc[j].nextElementSibling;
+                            if (pan.style.maxHeight) {
+                                pan.style.maxHeight = null;
+                                acc[j].style.backgroundColor = "#E5C175";
+                                acc[j].style.color = "#2D313D";
+                            } else {
+                                pan.style.maxHeight = pan.scrollHeight + "px";
+                            }
+                        }
+                    }
+                }
                 this.classList.toggle("active");
                 var panel = this.nextElementSibling;
                 if (panel.style.maxHeight) {
